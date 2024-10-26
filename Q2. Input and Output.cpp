e362d54d782b75880488487d1ed2b4f42b2313bd
Tanushwi Singh <156989068+Tanushwi@users.noreply.github.com>
2024-10-26 15:11:28 +0530

    Add files via upload

---
+++ Q2. Input and Output.cpp
@@ -0,0 +1,11 @@
+//Q. Add three numbers
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//    int a,b,c;
+//    cin>>a>>b>>c;
+//    cout<<a+b+c;
+//    return 0;
+//}
---
+++ Q3. Basic DataTypes.cpp
@@ -0,0 +1,20 @@
+//Q. Take input of datatypes:int,long,char,float,double and then setprecisions for float and double as per
+//	 requirement and then print all values in next line
+
+//#include<iostream>
+//#include<iomanip>
+//using namespace std;
+//int main()
+//{
+//    int a;
+//    long b;
+//    char c;
+//    float d;
+//    double e;
+//    cin>>a>>b>>c>>d>>e;
+//    cout<<a<<endl<<b<<endl<<c<<endl;
+//    cout<<fixed<<setprecision(3)<<d<<endl;
+//    cout<<fixed<<setprecision(9)<<e;
+//    return 0;
+//} 
+
