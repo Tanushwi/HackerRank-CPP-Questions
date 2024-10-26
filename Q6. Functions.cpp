eff7e50e626ca05e26c8381eaca76e407e4d4e2f
Tanushwi Singh <156989068+Tanushwi@users.noreply.github.com>
2024-10-26 20:17:25 +0530

    Add files via upload

---
+++ Q6. Functions.cpp
@@ -0,0 +1,49 @@
+//Q. Maximum out of 4 numbers using functions
+
+//#include<iostream>
+//using namespace std;
+//int max(int a,int b,int c,int d)
+//{
+//    if(a>=b)
+//    {
+//        if(a>=c)
+//        {
+//            if(a>=d)
+//                return a;
+//            else
+//                return d;
+//        }
+//        else
+//        {
+//            if(c>=d)
+//                return c;
+//            else
+//                return d;
+//        }
+//    }
+//    else
+//    {
+//        if(b>=c)
+//        {
+//            if(b>=d)
+//                return b;
+//            else
+//                return d;
+//        }
+//        else
+//        {
+//            if(c>=d)
+//                return c;
+//            else
+//                return d;
+//        }
+//    }
+//}
+//int main()
+//{
+//    int a,b,c,d;
+//    cin>>a>>b>>c>>d;
+//    int num=max(a,b,c,d);
+//    cout<<num;
+//    return 0;
+//}
---
+++ Q7. Pointer.cpp
@@ -0,0 +1,30 @@
+//Q. Find sum and difference of two numbers using pointers if diff>0 then print same else turn negative to positive
+//   Make functions of void datatype returning.
+
+//#include<iostream>
+//using namespace std;
+//void add(int *a,int *b)
+//{
+//    *a=*a+*b;
+//}
+//void difference(int *a,int *b)
+//{
+//    int diff=*a-*b;
+//    if(diff<=0)
+//        *a=diff*(-1);
+//    else
+//        *a=diff;
+//}
+//int main()
+//{
+//    int a,b;
+//    cin>>a;
+//    cin>>b;
+//    int temp=a;
+//    add(&a,&b);
+//    cout<<a<<endl;
+//    a=temp;
+//    difference(&a,&b);
+//    cout<<a;
+//    return 0;
+//}
